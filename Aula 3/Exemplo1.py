579e28413ae6e271dd253f1398f3d384fe0ff4b6
DougDg07 <dougfarah07@outlook.com>
2024-10-02 19:55:07 -0300

    Reorganização das pastas

---
+++ Aula 3/Exemplo1.py
@@ -0,0 +1,4 @@
+print("Vamos calcular o imposto!")
+valorproduto = float(input("coloque o valor do produto:"))
+imposto = valorproduto * 0.1
+print(f"O valor que você terá que pagar de imposto desse produto é:{imposto}")
