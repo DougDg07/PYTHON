aed674d78d05903082bf1c4b0678b857244c01df
DougDg07 <dougfarah07@outlook.com>
2024-10-02 20:30:51 -0300

    Inclusão do exercício do IPVA
    Exercício 1 - Aula 3 02/10

---
+++ Aula 3/Exercício - IPVA.py
@@ -0,0 +1,4 @@
+print("Vamos calcular o IPVA!")
+valorcarro = float(input("Qual o valor do carro:"))
+ipva = valorcarro * 0.04
+print(f"O valor que você terá que pagar de imposto desse produto é:{ipva}")
