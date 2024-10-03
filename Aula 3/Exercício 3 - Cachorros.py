56a1c06eabdf2e22dc07e2e54e86308d160b858a
DougDg07 <dougfarah07@outlook.com>
2024-10-02 21:42:02 -0300

    +1

--- Aula 3/Exercício 3 - Cachorros.py
+++ Aula 3/Exercício 3 - Cachorros.py
@@ -1,12 +1,9 @@
 print("Combo da cachorrada!!!")
-pata = float(input("Qual o tamanho da pata do seu pet:"))
 coleira = float(input("Digite o tamanho da coleira:"))
-sapato = int(pata * coleira*2)
+sapato = int(coleira * 2)
 print(f"O sapato ideal é: {sapato}")
 
-
 print("Agora vamos a fucinheira!!")
-fucinheira = float(input("Digite o tamanho da fucinheira:"))
 fucinheira = int(sapato * 3)
 print(f"A fucinheira perfeita é: {fucinheira}")
 
