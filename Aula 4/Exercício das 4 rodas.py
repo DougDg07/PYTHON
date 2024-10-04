38529d1b8f32ee8578b974059451362226d125b2
DougDg07 <dougfarah07@outlook.com>
2024-10-04 19:36:18 -0300

    Update Exercício das 4 rodas.py

--- Aula 4/Exercício das 4 rodas.py
+++ Aula 4/Exercício das 4 rodas.py
@@ -4,6 +4,19 @@ rodas = int(input("Quantas rodas tem seu carro:"))
 
 if (rodas == 4):
     print("Sim, é um carro ou van!")
+elif (rodas == 2):
+    print("O veiculo é uma moto ou bicicleta")
+elif (rodas == 1):
+    print("É a merda de um monociclo")
 else:
-    print("Não, não é um veiculo de 4 rodas!")
-     
+    print("Não foi encontrado um veiculo que corresponde ao número de rodas!")
+   
+# Quando temos mais de uma condicional, , usaremos o "elif" sintaxe: 
+    #If (condição): --> 1° condição
+    
+    #Elif(condição): --> 2° condição
+    #Elif(condição): --> 3° condição
+    #Elif(condição): --> 4° condição
+    #por ai vai...
+
+    #Else:  --> Ocorre quando nenhuma condição dos elif ou if for atendida  
