7671400d534b59a00e877c0ff680559e31896ebb
DougDg07 <dougfarah07@outlook.com>
2024-10-04 19:18:02 -0300

    Aula 4

---
+++ Aula 4/Exercício das 4 rodas.py
@@ -0,0 +1,9 @@
+# Faça um programa que pergunte quantas rodas tem um veiculo, se tiver 4 diga que é um carro ou van. 
+print("Carro ou Moto!")
+rodas = int(input("Quantas rodas tem seu carro:"))
+
+if (rodas == 4):
+    print("Sim, é um carro ou van!")
+else:
+    print("Não, não é um veiculo de 4 rodas!")
+     
---
+++ Aula 4/Validação do aluno.py
@@ -0,0 +1,9 @@
+# Faça um programa que valide se um aluno pode fazer o curso de python, onde  o único critério é ele ter idade maior ou igual que 16
+print("Validação!")
+idade = int(input("Digite sua idade:"))
+
+if (idade >= 16):
+    print("Sim, você pode fazer o curso!")
+else:
+    print("Não, não pode fazer o curso!")
+     
