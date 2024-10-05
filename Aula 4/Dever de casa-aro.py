477a97f2ff563a32682e5e5f47d40debb779e223
DougDg07 <dougfarah07@outlook.com>
2024-10-04 21:36:29 -0300

    Update Dever de casa-aro.py

--- Aula 4/Dever de casa-aro.py
+++ Aula 4/Dever de casa-aro.py
@@ -7,7 +7,7 @@ manete = aro / 4
 quadro = aro  # O tamanho do quadro é igual ao tamanho do aro
    
 # Exibe as recomendações
-print(f"Recomendações baseadas no tamanho do aro de {aro} polegadas:")
-print(f"Tamanho do guidão: {guidao:.2f} polegadas")
-print(f"Tamanho da manete: {manete:.2f} polegadas")
-print(f"Tamanho do quadro: {quadro:.2f} polegadas")
+print(f"Recomendações baseadas no tamanho do aro de {aro}:")
+print(f"Tamanho do guidão: {guidao:.2f}")
+print(f"Tamanho da manete: {manete:.2f}")
+print(f"Tamanho do quadro: {quadro:.2f}")
