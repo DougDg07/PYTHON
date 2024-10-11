017b83f84c2a452d638d2fdae6af09095f4edea2
DougDg07 <dougfarah07@outlook.com>
2024-10-11 20:52:44 -0300

    Atualização

--- Aula 7/Dever - Reciclagem 2.py
+++ Aula 7/Dever - Reciclagem 2.py
@@ -1,6 +1,4 @@
-
 print("Bem-vindo ao programa de reciclagem SENAC!")
-
 
 contagem = {
     'papel': 0,
@@ -12,35 +10,44 @@ contagem = {
 }
 
 while True:
-    
     material = input("Informe o tipo de material que deseja reciclar (papel, plástico, vidro, metal, orgânico ou resíduos não recicláveis): ").strip().lower()
 
-   
     if material == 'papel':
-        print("Descarte na lixeira azul.")
+        lixeira = "azul"
         contagem['papel'] += 1
     elif material == 'plástico':
-        print("Descarte na lixeira vermelha.")
+        lixeira = "vermelha"
         contagem['plástico'] += 1
     elif material == 'vidro':
-        print("Descarte na lixeira verde.")
+        lixeira = "verde"
         contagem['vidro'] += 1
     elif material == 'metal':
-        print("Descarte na lixeira amarela.")
+        lixeira = "amarela"
         contagem['metal'] += 1
     elif material == 'orgânico':
-        print("Descarte na lixeira marrom.")
+        lixeira = "marrom"
         contagem['orgânico'] += 1
     elif material == 'resíduos não recicláveis':
-        print("Descarte na lixeira cinza.")
+        lixeira = "cinza"
         contagem['não reciclável'] += 1
     else:
         print("Erro! Tente novamente e não contabilizar na contagem de nenhuma lixeira.")
+        continue  
 
-    
-    continuar = input("Deseja continuar reciclando? (s/n): ").lower()
+    resposta = input(f"Em qual lixeira vai o {material}? ").strip().lower()
+
+    if resposta == lixeira:
+        print(f"Você acertou! Seu lixo vai na lixeira {lixeira}.")
+    else:
+        print(f"Você errou! O lixo vai na lixeira {lixeira}. Gostaria de tentar de novo? (s/n): ")
+        tentar_novamente = input().strip().lower()
+        if tentar_novamente != 's':
+            break
+
+    continuar = input("Deseja continuar nosso jogo? (s/n): ").strip().lower()
     if continuar != 's':
         break
+
 
 print("\nResumo da reciclagem:")
 print("Papel:", contagem['papel'])
