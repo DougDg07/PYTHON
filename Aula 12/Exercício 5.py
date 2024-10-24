537adc6ce0c4c4e933c27d49d248ef831d22847b
DougDg07 <dougfarah07@outlook.com>
2024-10-23 21:55:21 -0300

    Aula 2 -2

---
+++ Aula 12/Exercício 5.py
@@ -0,0 +1,32 @@
+#Faça uma variável sorte onde ela multiplica o ataque por até 20 de ambos os personagens, aumente a vida deles em 10 vezes
+
+import time
+import random
+aventureiro = [
+    ["Vida", "Ataque base"],
+    [1000, 20]
+]
+assaltante = [
+    ["Vida", "Ataque base"],
+    [600, 20]
+]
+
+while assaltante[1][0] > 0 and aventureiro[1][0] > 0:
+    Variavelaventureiro = random.randint(1, 20)
+    Variaveassaltante = random.randint(1, 20)
+    assaltante[1][0] -= 20 * Variaveassaltante
+    aventureiro[1][0] -= 20 * Variaveassaltante
+    print("Status aventureiro:")
+    for linha in aventureiro:
+        print(linha)
+    print(f"multiplicador de critico:", Variavelaventureiro)
+    print("Status assaltante:")
+    for linha in assaltante:
+        print(linha)
+    print(f"multiplicador de critico:", Variaveassaltante)
+    time.sleep(4)
+
+
+print("\nBatalha finalizada!")
+print(f"Aventureiro - Vida final: {aventureiro[1][0]}")
+print(f"Assaltante - Vida final: {assaltante[1][0]}")
