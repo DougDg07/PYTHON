0a2b9ed43df06c162c3bdffcbad1935c57fb30b0
DougDg07 <dougfarah07@outlook.com>
2024-11-30 21:06:42 -0300

    Update Lava rapido2.py

--- Aula 5/Lava rapido2.py
+++ Aula 5/Lava rapido2.py
@@ -2,7 +2,10 @@ print("Escolha o tipo de lavagem:")
 print("1 - Lavagem Completa a R$50,00")
 print("2 - Lavagem Básica a R$35,00")
 
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 escolha = int(input("Digite o número 1 para Lavagem Completa e 2 para Lavagem Básica: "))
 
 if escolha == 1:
@@ -24,7 +27,10 @@ if pretinho == "s":
 else:
     print("Sem problemas, deixamos para próxima vez! =)")
 
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 print(f"Serviço: {tipo_servico}")
 print(f"Valor total: R${valor:.2f}")
 
